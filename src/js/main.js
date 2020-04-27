86b4293b7402417038d391ea5b0fd62c822054ce
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-04-27 22:53:33 +0200

    feat: added GitHub API links to the project section

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,29 @@ if ('serviceWorker' in navigator) {
 console.log(`Hello world!`);
 
 
+
+const list = document.querySelector('.projects__list--js')
+fetch('https://api.github.com/users/kwiatkowski1981/repos?sort=updated&direction=asc')
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        console.log(repos);
+        for (const repo of repos) {
+            const {description, name, html_url, download_html, } = repo
+
+            list.innerHTML += 
+            `<li class="projects__list--element">
+                    <a href="${html_url}" class="projects__nav--list"> ${name}</a> 
+                                         &nbsp  &nbsp &nbsp  
+                     <a href="${html_url}/archive/master.zip" class="projects__nav--download"> download </a> 
+                     <div style="clear: both;"> </br>
+                     <p>${description}</p>
+             </li>`
+            console.log(`it's done`);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
