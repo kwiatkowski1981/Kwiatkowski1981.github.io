40749b0d97b749633b911270ec081c1435df89f6
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-04-28 05:43:01 +0200

    feat: added new styles to project section

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -18,31 +18,41 @@ if ('serviceWorker' in navigator) {
 
 
 console.log(`Hello world!`);
-
-
+//  link do sciagania repo
+//  <a href="${html_url}/archive/master.zip" class="projects__nav--download"> download </a> 
 
 const list = document.querySelector('.projects__list--js')
 fetch('https://api.github.com/users/kwiatkowski1981/repos?sort=updated&direction=asc')
-    .then(resp => resp.json())
-    .then(resp => {
-        const repos = resp;
-        console.log(repos);
-        for (const repo of repos) {
-            const {description, name, html_url, download_html, } = repo
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    console.log(repos);
+    for (const repo of repos) {
+      const { description, name, html_url, download_html, } = repo
 
-            list.innerHTML += 
-            `<li class="projects__list--element">
-                    <a href="${html_url}" class="projects__nav--list"> ${name}</a> 
-                                         &nbsp  &nbsp &nbsp  
-                     <a href="${html_url}/archive/master.zip" class="projects__nav--download"> download </a> 
+      list.innerHTML +=
+        `<li class="projects__list--element">
+            <div class="api__main">
+            <img src="../assets/img/git1.svg" alt="Github logo" class="api__logo">
+            <h4 class="api__heading4"> ${name}</h4>
+            <p class="api__paragraph"> ${description}</p>
+            </div>
+            <div class="api__footer">
+                    <a href="${html_url}" class="projects__nav--list"> 
+                    <img src="../assets/img/code1.svg" alt="code icon" class="api__code">
+                    </a>
+                    <a href="${html_url}" class="projects__nav--list">
+                    <img src="../assets/img/demo1.svg" alt="small pc" class="api__demo">
+                    </a> 
+                   
                      <div style="clear: both;"> </br>
-                     <p>${description}</p>
+               </div>      
              </li>`
-            console.log(`it's done`);
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    })
+      console.log(`it's done`);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 
