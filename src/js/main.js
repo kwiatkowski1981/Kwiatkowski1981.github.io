e739f04918b6f28e6803c3a9d810e74042be7b50
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-03-14 15:40:53 +0100

    changed all api card, removed apiDemoLinks if the are null on gitHub

--- src/js/main.js
+++ src/js/main.js
@@ -3,44 +3,46 @@
 // service worker registration - remove if you're not going to use it
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("serviceworker.js").then(
-      function(registration) {
-        // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("serviceworker.js").then(
+            function (registration) {
+                // Registration was successful
+                console.log(
+                    "ServiceWorker registration successful with scope: ",
+                    registration.scope
+                );
+            },
+            function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            }
         );
-      },
-      function(err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
+    });
 }
 
 // place your code below
 
-console.log(`Hi there. It's nice to see You here.`);
-//  link do sciagania repo
-//  <a href="${html_url}/archive/master.zip" class="projects__nav--download"> download </a>
+console.log(` %c Hi there. It's nice to see You here.`, "color: yellow");
+
 
 const list = document.querySelector(".api__list--js");
+
 fetch(
-  "https://api.github.com/users/kwiatkowski1981/repos?sort=updated&direction=asc"
+    "https://api.github.com/users/kwiatkowski1981/repos?sort=updated&direction=asc"
 )
-  .then(resp => resp.json())
-  .then(resp => {
-    const repos = resp;
-    for (const repo of repos) {
-      const { description, name, html_url, download_html, homepage } = repo;
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            const {description, name, html_url, homepage} = repo;
+            const showDesription = description !== null ? description : "";
 
-      list.innerHTML += `<li class="api__list--element"> 
-            <div class="api__main">
-            
-
-            <svg  viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="api__logo">
+            const liElement = document.createElement('li');
+            liElement.classList.add('api__list--element');
+            const divApiMain = document.createElement('div');
+            divApiMain.classList.add('api__main');
+            const apiImageSvg = document.createElement('api__main-svg');
+            apiImageSvg.innerHTML = `<svg  viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="api__logo">
             <path d="M19.9844 2.97925C10.3242 2.97534 2.5 10.7957 2.5 20.448C2.5 28.0808 7.39453 34.5691 14.2109 
             36.9519C15.1289 37.1824 14.9883 36.53 14.9883 36.0847V33.0574C9.6875 33.6785 9.47266 30.1707 9.11719 
             29.5847C8.39844 28.3582 6.69922 28.0457 7.20703 27.4597C8.41406 26.8386 9.64453 27.616 11.0703 29.7214C12.1016 
@@ -52,47 +54,64 @@ fetch(
             28.3699C23.9639 28.8338 24.3385 29.3871 24.5941 29.9974C24.8496 30.6078 24.981 31.2629 24.9805 
             31.9246V36.3191C25.0117 36.6707 24.9805 37.0183 25.5664 37.0183C32.4844 34.6863 37.4648 28.1511 37.4648 
             20.4519C37.4648 10.7957 29.6367 2.97925 19.9844 2.97925Z" fill="#171F3F"/>
-            </svg>
+            </svg>`;
+            const apiHeading4 = document.createElement('h4');
+            apiHeading4.classList.add('api__heading4');
+            apiHeading4.textContent = `${name}`;
+            const apiParagraph = document.createElement('p');
+            apiParagraph.classList.add('api__paragraph');
+            apiParagraph.textContent = `${showDesription}`;
+            const apiFooterDiv = document.createElement('div');
+            apiFooterDiv.classList.add('api__footer');
+            const apiDemoLink = document.createElement('div');
+            apiDemoLink.classList.add('api__demo__link');
+            apiDemoLink.classList.add('api__nav--list1');
+            apiDemoLink.innerHTML = `<a href="${homepage}" class="api__nav--list1" class="api__naw--list1-js" >
+                         <p class="api__paragraph--link api__paragraph--link2">
+                         <img src="../assets/img/demo1.svg" alt="small pc icon" class="api__demo"> Demo</p>
+                         </a>`;
+
+            const apiGithubLink = document.createElement('api__github__link');
+            apiGithubLink.classList.add('api__github__link');
+            apiGithubLink.classList.add('api__nav--list2');
+            apiGithubLink.innerHTML = `<a href="${html_url}" class="api__nav--list2"> 
+                           <p class="api__paragraph--link">
+                           <img src="../assets/img/code1.svg" alt="code icon" class="api__code">  GitHub </p>
+                           </a> `;
+
+            list.appendChild(liElement);
+            liElement.appendChild(divApiMain);
+            divApiMain.appendChild(apiImageSvg);
+            divApiMain.appendChild(apiHeading4);
+            divApiMain.appendChild(apiParagraph);
+            liElement.appendChild(apiFooterDiv);
+
+            if (homepage !== null) {
+
+            apiFooterDiv.appendChild(apiDemoLink);
+            }
+            apiFooterDiv.appendChild(apiGithubLink);
 
 
+// list.innerHTML ******************************************************************************************************
 
-      
-            <h4 class="api__heading4"> ${name}</h4>
-            <p class="api__paragraph"> ${description}</p>
-            </div>
-            <div class="api__footer">
-            
-            <a href="${homepage}" class="api__nav--list1"> <p class="api__paragraph--link api__paragraph--link2"> 
-            <img src="../assets/img/demo1.svg" alt="small pc icon" class="api__demo"> Demo </p>
-            </a>
-            
-            
-            <a href="${html_url}" class="api__nav--list2"> <p class="api__paragraph--link">
-            <img src="../assets/img/code1.svg" alt="code icon" class="api__code">  GitHub </p>
-            </a> 
+            console.log(`API worked correctly, all jobs are done.`);
 
-               </div>      
-             </li>`;
-      console.log(`API worked correctly, all jobs are done.`);
-    }
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-
-
-
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 const arrowup = document.querySelector(".arrowup");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    arrowup.classList.add("arrowup--visible");
-    arrowup.classList.remove("arrowup");
-  } else {
-    arrowup.classList.remove("arrowup--visible");
-    arrowup.classList.add("arrowup");
-  }
+    if (window.pageYOffset > 100) {
+        arrowup.classList.add("arrowup--visible");
+        arrowup.classList.remove("arrowup");
+    } else {
+        arrowup.classList.remove("arrowup--visible");
+        arrowup.classList.add("arrowup");
+    }
 });
-console.log(`pozdro dla Matiego (https://maczi01.github.io/) z podziękowaniem za scrolera 😂`)
+console.log(`%c pozdro dla %c Matiego %c (https://maczi01.github.io/) z podziękowaniem za scrolera 😂`, "color: yellow", "color: red", "color: yellow")
